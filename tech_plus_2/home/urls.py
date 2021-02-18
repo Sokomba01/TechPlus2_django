eaf8e64e20e889f8452ebb2293d5dcd5267a0ef0
razaali4939 <48799003+razaali4939@users.noreply.github.com>
2021-02-19 00:06:30 +0500

    3rd Commit

--- tech_plus_2/home/urls.py
+++ tech_plus_2/home/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path
-from home import  views
+from . import  views
 urlpatterns = [
-    path("", views.index, name='home'),
+    path('', views.index, name='register'),
+    path('home/', views.SignUp, name='home'),
 ]
--- tech_plus_2/home/views.py
+++ tech_plus_2/home/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render, HttpResponse
-
+from  .models import MyUsers
 # Create your views here.
+from w3lib import form
+
+
 def index(request):
     return render(request, 'signup.html')
+
+
+def SignUp(request):
+    if request.method == 'POST':
+        fname = request.POST["fname"]
+        lname = request.POST["lname"]
+        email = request.POST["email"]
+        portExistingNumber = request.POST["portExistingNumber"]
+        password = request.POST["password"]
+        attachedDocument = request.POST["attachedDocument"]
+        ins = MyUsers(fName = fname, lName = lname, email = email, password = password, portExistingNumber = portExistingNumber, sharedDocument = attachedDocument)
+        ins.save()
+    return render(request, 'index.html')
